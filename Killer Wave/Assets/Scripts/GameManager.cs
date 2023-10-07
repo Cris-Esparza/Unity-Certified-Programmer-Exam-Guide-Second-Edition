f7b9699043e495163ed4ec4b47f5a123e9043ce3
Cris-Esparza <cesparza@uccs.edu>
2023-10-06 22:28:38 -0600

    chapter 7 done

--- Killer Wave/Assets/Scripts/GameManager.cs
+++ Killer Wave/Assets/Scripts/GameManager.cs
@@ -45,8 +45,8 @@ public class GameManager : MonoBehaviour
     {
         switch (sceneNumber)
         {
-            // testLevel, level1, level2, level3
-            case 3: case 4: case 5: case 6:
+            // level1, level2, level3
+            case 3: case 4: case 5:
                 {
                     LightSetup();
                     CameraSetup();
---
+++ Killer Wave/Assets/Scripts/LoadSceneComponent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneComponent : MonoBehaviour
+{
+    float timer = 0;
+    public string loadThisScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.GetComponentInChildren<ScoreManager>().resetScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 3)
+        {
+            SceneManager.LoadScene(loadThisScene);
+        }
+    }
+}
